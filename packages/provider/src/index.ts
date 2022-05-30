cc47ceea6fb8fd38974c8a72dd2e40e0a8579b63
Karibash <y.karibayashi@gmail.com>
2022-05-31 00:24:36 +0900

    Add a module that reads classes from the directory and provide them

---
+++ packages/provider/src/index.ts
@@ -0,0 +1,42 @@
+import { DynamicModule, Module, Provider } from '@nestjs/common';
+import fs from 'fs/promises';
+
+export type ProviderModuleOptions = Pick<DynamicModule, 'global'> & {
+  path: string;
+  fileNameRegExp?: RegExp;
+  exportNameRegExp?: RegExp;
+};
+
+@Module({})
+export class ProviderModule {
+  static async forRootAsync(options: ProviderModuleOptions): Promise<DynamicModule> {
+    const fileNames = await fs.readdir(options.path);
+
+    const modules = await Promise.all(
+      fileNames
+        .filter(name => options.fileNameRegExp?.test(name) ?? true)
+        .map(name => `${options.path}/${name.replace(/.(js|ts)$/, '')}`)
+        .map<unknown>(async path => await import(path)),
+    );
+
+    const providers = modules.reduce<Provider[]>((previous, current) => {
+      if (!current || typeof current !== 'object') return previous;
+
+      previous.push(...Object.entries(current).reduce<Provider[]>((previous, [key, value]) => {
+        if (options.exportNameRegExp?.test(key) ?? true) {
+          previous.push(value);
+        }
+        return previous;
+      }, []));
+
+      return previous;
+    }, []);
+
+    return {
+      ...options,
+      module: ProviderModule,
+      providers: providers,
+      exports: providers,
+    };
+  }
+}
